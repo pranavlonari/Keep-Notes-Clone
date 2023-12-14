01c2e41da2862beca64243f697581d797b0e722e
pranavlonari <pranavlonari12@gmail.com>
2023-12-14 18:14:24 +0530

    added notes.js file some predefined data

---
+++ notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    key: 1,
+    title: "Meeting Agenda",
+    content:
+      "Prepare for the weekly team meeting. Discuss project updates, upcoming deadlines, and assign tasks for the week.",
+  },
+  {
+    key: 2,
+    title: "Client Call Notes",
+    content:
+      "Summary of the client call with XYZ Corporation. Addressed concerns regarding project timeline and clarified deliverables.",
+  },
+  {
+    key: 3,
+    title: "Project Milestones",
+    content:
+      "Outline the key milestones for the upcoming project. Review and update the timeline to ensure timely completion.",
+  },
+  {
+    key: 4,
+    title: "Tech Conference Ideas",
+    content:
+      "Brainstorm ideas for the upcoming tech conference. Consider potential topics, speakers, and logistics.",
+  },
+];
+
+export default notes;
